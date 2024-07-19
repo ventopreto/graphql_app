e366bb5699e643c4a54b5303482f5d1a4e650c37
ventopreto <luiz.anjos@rebase.com.br>
2024-07-19 18:55:06 -0300

    cria endpoint de consulta da policy

--- app/graphql/types/policy_type.rb
+++ app/graphql/types/policy_type.rb
@@ -3,8 +3,8 @@
 module Types
   class PolicyType < Types::BaseObject
     field :id, ID, null: false
-    field :start_date, GraphQL::Types::ISO8601DateTime, null: false
-    field :end_date, GraphQL::Types::ISO8601DateTime, null: false
+    field :start_date_coverage, GraphQL::Types::ISO8601DateTime
+    field :end_date_coverage, GraphQL::Types::ISO8601DateTime
     field :created_at, GraphQL::Types::ISO8601DateTime
     field :updated_at, GraphQL::Types::ISO8601DateTime
     field :insured_id, ID, null: false
